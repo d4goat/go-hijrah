514ca9435b2a0601faee22e7f83157767d0ddf4d
d4goat <kenzoalgundul@gmail.com>
2024-11-27 22:28:50 +0700

    add: vercel.json\

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    vercel()
   ],
   resolve: {
     alias: {
